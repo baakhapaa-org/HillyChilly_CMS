96aedbb911438a36c9765c13d6445d0058a2e160
sushantDev <sushantsapkota07@gmail.com>
2026-07-18 23:49:27 +0545

    fix: use correct View namespace for ShareErrorsFromSession

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -16,8 +16,8 @@ use Filament\Widgets\{AccountWidget, FilamentInfoWidget};
 use Illuminate\Cookie\Middleware\{AddQueuedCookiesToResponse, EncryptCookies};
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\{ShareErrorsFromSession, StartSession};
-use Illuminate\View\Middleware\ShareErrorsFromSession as ShareErrors;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class AdminPanelProvider extends PanelProvider
 {
